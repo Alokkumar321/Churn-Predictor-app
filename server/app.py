df2df7d083a5804fe057b5d11a0284959d4edbaa
Alok Kumar <alokkumar80777@gmail.com>
2025-08-05 21:19:08 +0530

    Prepare code for Railway deployment

--- server/app.py
+++ server/app.py
@@ -67,5 +67,7 @@ def download_file():
     else:
         return jsonify({'error': 'File not found'}), 404
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
